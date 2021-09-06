e9109cd4554cc7e63e3372b4335f955075f40a5f
Ubuntu19.10 <vjolashtishi@gmail.com>
2021-09-06 17:26:21 +0200

    Listing Tasks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
+
+
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: true
+    }
+  ])
   return (
     <div className="container">
-     <h1>Hello {name}!</h1>
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,10 @@
+
+const Tasks = ({ tasks }) => {
+    return (
+        <>
+            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+        </>
+    )
+}
+
+export default Tasks
