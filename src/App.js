69b1df7a75f266446d4e1971f9eaba5f8972ce19
OXA <vjolashtishi@gmail.com>
2021-09-07 15:04:42 +0200

    Toggling Reminder

--- src/App.js
+++ src/App.js
@@ -24,10 +24,19 @@ function App() {
       reminder: true
     }
   ])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+         setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,11 @@
+import Task from './Task'
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
-            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+            {tasks.map((task) => (
+                <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
+            ))}
         </>
     )
 }
